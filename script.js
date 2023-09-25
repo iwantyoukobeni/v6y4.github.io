ac4824f7b0c66657ac720472ffdcc7c74767d1b2
v6y4 <121084767+v6y4@users.noreply.github.com>
2023-09-24 20:49:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-function toggleLinks() {
-  document.getElementById("links-container").classList.toggle("show-links");
-}
+new Vue({
+  el: '#app',
+  methods: {
+    toggleLinks() {
+      document.getElementById("links-container").classList.toggle("show-links");
+    }
+  },
+  data: {
+    spec: {
+      host: "https://www.google.com",
+      title: "Google Search",
+      description: "Hello, API",
+      opened: true,
+      request: [{
+        method: 'get',    // post, delete or put 
+        description: "Google Home",
+        url: "/"
+      }]
+    }
+  }
+})
+
 
